d0ad216c1df29d6fd85a5d89bb64adfed0d6ede8
Xuefei Cheng <xfcheng0122@gmail.com>
2018-10-18 23:41:20 -0400

    query multi cities's weather

--- test_weather.py
+++ test_weather.py
@@ -1,6 +1,9 @@
 import requests
 
 APIKEY = '011d892e19d82635af74f10ed45ae6b7'
-resp = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Boston,us&APPID={0}'.format(APIKEY))
-if resp.status_code == requests.codes.ok:
-    print(resp.json())
+cities = [{'city':'Boston','contry':'us'},{'city':'Beijing','contry':'cn'},{'city':'Chongqing','contry':'cn'}]
+for item in cities:
+    resp = requests.get('http://api.openweathermap.org/data/2.5/weather?q={0},{1}&APPID={2}'.format(item['city'], item['contry'], APIKEY))
+    if resp.status_code == requests.codes.ok:
+        print(item['city'], item['contry'])
+        print(resp.json())
